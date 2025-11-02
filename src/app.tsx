e0f7165471cf15ff52dec02ed21ec1d82b2a6712
David URVOY <dev@david.urvoy.email>
2025-11-03 00:12:41 +0100

    chore: search params for 3D version

--- src/app.tsx
+++ src/app.tsx
@@ -3,9 +3,14 @@ import { Canvas } from '@react-three/fiber'
 import { Suspense } from 'react'
 
 export function App() {
+    const is3DActive = window.location.search.includes('3d=true')
+
     return (
         <div className="fixed top-0 left-0 h-full w-full">
-            <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
+            <Canvas
+                hidden={!is3DActive}
+                camera={{ position: [0, 0, 5], fov: 50 }}
+            >
                 <Suspense fallback={null}>
                     <Experience />
                 </Suspense>
